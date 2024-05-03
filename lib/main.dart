7d94009f4481e454823af9c5ea3442ffb38c37bd
Lans12345 <olanalans12345@gmail.com>
2024-05-04 07:30:58 +0800

    wip landing screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:cue_bar/firebase_options.dart';
 import 'package:cue_bar/screens/home_screen.dart';
+import 'package:cue_bar/screens/landing_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Cue Bar and Billiards',
-      home: HomeScreen(),
+      home: LandingScreen(),
     );
   }
 }
