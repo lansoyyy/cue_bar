32f0e9428ef33698ed864aab6c35b14d4b2e4f89
Lans12345 <olanalans12345@gmail.com>
2024-05-04 07:34:23 +0800

    added landing screen

--- lib/screens/landing_screen.dart
+++ lib/screens/landing_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cue_bar/widgets/button_widget.dart';
 import 'package:flutter/material.dart';
 
 class LandingScreen extends StatelessWidget {
@@ -5,6 +6,31 @@ class LandingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black54,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Image.asset(
+              'assets/images/123.jpg',
+            ),
+          ),
+          const SizedBox(
+            height: 200,
+          ),
+          Center(
+            child: ButtonWidget(
+              textColor: Colors.black,
+              color: Colors.white,
+              label: 'Get Started',
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
